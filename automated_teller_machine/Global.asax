ec3aeaeab87495483ee8624a68f75c4e9ae24992
Steven Hardy <steverhardy@gmail.com>
2019-07-30 10:06:22 -0400

    Initial Commit

---
+++ automated_teller_machine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="automated_teller_machine.MvcApplication" Language="C#" %>
---
+++ automated_teller_machine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(automated_teller_machine.Startup))]
+namespace automated_teller_machine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
